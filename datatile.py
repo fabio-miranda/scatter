45548e798af0fb0e87e0b78a226a212fe412485d
Fabio Miranda <fabiom@gmail.com>
2013-06-06 01:17:31 -0600

    Distribution revised

--- datatile.py
+++ datatile.py
@@ -15,7 +15,7 @@ def generateData(numentries, numdim):
   for i in range(0, numentries):
     entry = []
     for j in range(0, numdim):
-      entry.append(random.uniform(0, 1))
+      entry.append(random.uniform(0,1))
     aux.append(entry)
 
   data = numpy.array(aux)
@@ -23,12 +23,21 @@ def generateData(numentries, numdim):
 
 def createDataTile(binsize, width, height, dimension1, dimension2):
 
-  buf = numpy.zeros(shape=(width/binsize,height/binsize))
+  buf = numpy.zeros(shape=(width/binsize,height/binsize, 4))
 
   for i in range(0, width/binsize):
     for j in range(0, height/binsize):
       #TODO: handle binsize
-      buf[i, j] = dimension1[i] + dimension2[j]
+      #if(dimension1[i] > 0):
+        buf[i, j, 0] += 1
+        buf[i, j, 1] += 1
+        buf[i, j, 2] += 1
+        buf[i, j, 3] += 1
+      #if(dimension2[i] > 0):
+        buf[i, j, 0] += 1
+        buf[i, j, 1] += 1
+        buf[i, j, 2] += 1
+        buf[i, j, 3] += 1
 
 
   return cairo.ImageSurface.create_for_data(buf, cairo.FORMAT_ARGB32, width/binsize, height/binsize)
