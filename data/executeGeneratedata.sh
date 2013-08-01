05a42bfac537949869e8983373f319d3786a101e
Fabio Markus Miranda <fabiom@gmail.com>
2013-08-01 14:24:04 -0600

    Data tiles script

---
+++ data/executeGeneratedata.sh
@@ -0,0 +1,10 @@
+mkdir ./outlets/datatiles
+../generatedata.o 816 2 1 ./outlets/datatiles/ ./outlets/data.csv
+mkdir ./wine/datatiles
+../generatedata.o 178 14 0 ./wine/datatiles/ ./wine/wine.data
+mkdir ./iris/datatiles
+../generatedata.o 150 5 0 ./iris/datatiles/ ./iris/data.csv
+mkdir ./faithful/datatiles
+../generatedata.o 272 2 1 ./faithful/datatiles/ ./faithful/faithful.dat
+mkdir ./auto-mpg/datatiles
+../generatedata.o 392 9 1 ./auto-mpg/datatiles/ ./auto-mpg/auto-mpg.data
