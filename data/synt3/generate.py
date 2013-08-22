91e165ba3b83d981d4700ec769dcb04c8a2d5fe6
Fabio Miranda <fabiom@gmail.com>
2013-08-22 10:36:40 -0600

    Synt3 data

--- data/synt3/generate.py
+++ data/synt3/generate.py
@@ -2,46 +2,31 @@ import sys
 import math
 import random
 
+count = 0
+def circle(innerradius, outterradius, numrotations):
+  global count
+  delta = math.pi/32.0
+  for i in range(0, numrotations):
+    j = 0
+    while(j < 2.0 * math.pi):
+      x = random.uniform(innerradius,outterradius) * math.cos(j)
+      y = random.uniform(innerradius,outterradius) * math.sin(j)
+
+      f.write(str(x)+';'+str(y)+'\n')
+      count+=1
+
+      j+=delta
+
 
 #if(len(sys.argv) <= 1):
   #print 'Error! What is the csv file to parse? First line must be the fields names'
   #exit(1)
 
-numrotations = 100
-delta = math.pi/8.0
 f = open('./data', 'w')
-
-#f.write('0;0\n')
-#f.write('1;0\n')
-#f.write('0;1\n')
-#f.write('1;1\n')
-count=0
-
-radius = 0.1
-for i in range(0, numrotations):
-  j = 0
-  while(j < 2.0 * math.pi):
-    x = random.uniform(0.1,0.2) * math.cos(j)
-    y = random.uniform(0.1,0.2) * math.sin(j)
-
-    f.write(str(x)+';'+str(y)+'\n')
-    count+=1
-
-    j+=delta
-
-numrotations = 5
-radius = 0.5
-delta = math.pi/32.0
-for i in range(0, 2*numrotations):
-  j = 0
-  while(j < 2.0 * math.pi):
-    x = random.uniform(0.5,1.0) * math.cos(j)
-    y = random.uniform(0.5,1.0) * math.sin(j)
-
-    f.write(str(x)+';'+str(y)+'\n')
-    count+=1
-
-    j+=delta
+circle(0.1, 0.2, 10)
+circle(0.5, 0.7, 10)
+circle(0.8, 0.9, 10)
+circle(1.0, 1.1, 1)
 
 f.close()
 
