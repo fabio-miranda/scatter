23e1070418712e606c3da9cf9cd2866dc0846980
Fabio Miranda <fabiom@gmail.com>
2013-08-22 01:05:37 -0600

    Flight data only considering Continental USA

--- data/flights/parseairport.py
+++ data/flights/parseairport.py
@@ -28,49 +28,58 @@ lat1 = -float('inf')
 lng1 = -float('inf')
 for line in f:
 	tokens = line.split(',')
-	for i in range(0, 6):
-		out.write(tokens[i]+',')
+	#for i in range(0, 6):
+		#out.write(tokens[i]+',')
 
-	#origin
 	origin = tokens[6]
-	lat = airports[origin]['lat']
-	lng = airports[origin]['lng']
-	out.write(str(lat)+','+str(lng)+',')
+	olat = airports[origin]['lat']
+	olng = airports[origin]['lng']
 
-	if(lat < lat0):
-		lat0 = lat
-	if(lng < lng0):
-		lng0 = lng
-	if(lat > lat1):
-		lat1 = lat
-	if(lng > lng1):
-		lng1 = lng
-
-	#dest
 	dest = tokens[7]
-	lat = airports[dest]['lat']
-	lng = airports[dest]['lng']
-	out.write(str(lat)+','+str(lng)+',')
+	dlat = airports[dest]['lat']
+	dlng = airports[dest]['lng']
 
-	if(lat < lat0):
-		lat0 = lat
-	if(lng < lng0):
-		lng0 = lng
-	if(lat > lat1):
-		lat1 = lat
-	if(lng > lng1):
-		lng1 = lng
+	#just use flights within Continental USA
+	if(olng >= -124.7625 and olng <= -66.9326 and olat >= 24.5210 and olat <= 49.3845 and dlng >= -124.7625 and dlng <= -66.9326 and dlat >= 24.5210 and dlat <= 49.3845):
+		#origin
+		#out.write(str(lat)+','+str(lng)+',')
+		out.write(str(olat)+';'+str(olng)+'\n')
+		count+=1
 
-	out.write(tokens[8]+',')
-	out.write(tokens[9]+'\n')
-	count+=1
+		if(olat < lat0):
+			lat0 = olat
+		if(olng < lng0):
+			lng0 = olng
+		if(olat > lat1):
+			lat1 = olat
+		if(olng > lng1):
+			lng1 = olng
+
+		#dest
+		#out.write(str(lat)+','+str(lng)+',')
+		out.write(str(dlat)+';'+str(dlng)+'\n')
+		count+=1
+
+		if(dlat < lat0):
+			lat0 = dlat
+		if(dlng < lng0):
+			lng0 = dlng
+		if(dlat > lat1):
+			lat1 = dlat
+		if(dlng > lng1):
+			lng1 = dlng
+
+	#out.write(tokens[8]+',')
+	#out.write(tokens[9]+'\n')
+	
 
 out.close()
 f.close()
 
 info = open('./info.txt', 'w')
 info.write('numentries: '+str(count)+'\n')
-info.write('numdim: '+str(numdim)+'\n')
+#info.write('numdim: '+str(numdim)+'\n')
+info.write('numdim: 2\n')
 info.write('min: 0\n')
 info.write('max: 1\n')
 info.write('isline: 1\n')
